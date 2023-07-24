67c696f93e78241d576948fceeac8eb64d9a9894
liudawei <liudawei@zhidemai.com>
2023-07-24 16:51:22 +0800

    init

---
+++ src/pages/page1/App.vue
@@ -0,0 +1,12 @@
+<template>我是page1模块</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 import vue from '@vitejs/plugin-vue'
+import { resolve, join } from "path";
+import { readdirSync } from "fs";
 
+const project_pages = {};
+const entryPath = resolve(__dirname, "./src/pages");
+const entrys = readdirSync(entryPath).reduce((obj, dirname) => {
+  obj[dirname] = join(entryPath, dirname);
+  return obj;
+}, {});
+Object.keys(entrys).forEach(pageName => {
+  project_pages[pageName] = resolve(__dirname, `src/pages/${pageName}/index.html`);
+});
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  let pages = {};
+  const env = loadEnv(mode, process.cwd());
+  const isDev = mode === "development";
+  console.log(mode);
+  if (isDev) {
+    pages = { ...project_pages };
+  } else {
+    if (env.VITE_APP_MODEL) {
+      pages[env.VITE_APP_MODEL] = project_pages[env.VITE_APP_MODEL];
+    } else {
+      pages = { ...project_pages };
+    }
+  }
+  return {
+    plugins: [vue()],
+    build: {
+      rollupOptions: {
+        input: pages,
+        output: { dir: "./dist" }
+      }
+    },
+    resolve: {
+      extensions: [".js", ".ts", ".vue", ".json"],
+      alias: {
+        "@": resolve(__dirname, "src"),
+        "@page1": resolve(__dirname, "src/pages/page1"),
+        "@page2": resolve(__dirname, "src/pages/page2"),
+      },
+    },
+  }
 })
