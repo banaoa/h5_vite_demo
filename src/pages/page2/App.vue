0419d3fc3fe051291f5a2e0f0fb77171e7a42a00
liudawei <liudawei@zhidemai.com>
2023-07-25 17:27:18 +0800

    增加lint

--- src/pages/page2/App.vue
+++ src/pages/page2/App.vue
@@ -1,6 +1,6 @@
-<script lang="ts">
+<script setup lang="ts">
 const aaa = 123;
-console.log(123);
+console.log(aaa);
 </script>
 <template>我是page2模块</template>
 
@@ -8,10 +8,11 @@ console.log(123);
 #app {
   height: 100%;
   color: #2c3e50;
+  font-size: 12px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
   border: 1px solid green;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
 }
 </style>
