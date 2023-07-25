19e2f6137f17f63e3563fcb502539cc02f5dfeff
liudawei <liudawei@zhidemai.com>
2023-07-25 18:03:02 +0800

    增加lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,20 +1,20 @@
 // lint-staged规则
-module.exports = {
+export default {
   // ts文件走eslint,tsc  TODO, ts文件走vue-tsc也行吧???
   'src/**/*.{ts,tsx}': [
     'prettier --write',
-    'eslint -c .eslintrc.js --fix',
+    'eslint -c .eslintrc.json --fix',
     () => 'tsc --noEmit --skipLibCheck',
   ],
   // vue文件走eslint, stylelint, vue-tsc
   'src/**/*.vue': [
     'prettier --write',
-    'eslint -c .eslintrc.js',
-    'stylelint --config stylelint.config.js',
+    'eslint -c .eslintrc.json',
+    'stylelint --config .stylelintrc.json',
     () => 'vue-tsc --noEmit --skipLibCheck',
   ],
   // js文件走eslint
   'src/**/*.{json,js,jsx}': ['prettier --write', 'eslint --fix'],
   // scss文件走stylelint
-  'src/**/*.{css,scss}': ['prettier --write', 'stylelint --config stylelint.config.js --fix'],
-}
+  'src/**/*.{css,scss}': ['prettier --write', 'stylelint --config .stylelintrc.json --fix'],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,45 @@
-import { join, resolve } from "node:path";
-import { readdirSync } from "node:fs";
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { join, resolve } from 'node:path';
+import { readdirSync } from 'node:fs';
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslint from 'vite-plugin-eslint';
 
 const project_pages = {};
-const entryPath = resolve(__dirname, "./src/pages");
+const entryPath = resolve(__dirname, './src/pages');
 const entrys = readdirSync(entryPath).reduce((obj, dirname) => {
   obj[dirname] = join(entryPath, dirname);
   return obj;
 }, {});
 Object.keys(entrys).forEach((pageName) => {
-  project_pages[pageName] = resolve(
-    __dirname,
-    `src/pages/${pageName}/index.html`,
-  );
+  project_pages[pageName] = resolve(__dirname, `src/pages/${pageName}/index.html`);
 });
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   let pages = {};
-  // eslint-disable-next-line n/prefer-global/process
   const env = loadEnv(mode, process.cwd());
-  const isDev = mode === "development";
+  const isDev = mode === 'development';
   if (isDev) {
     pages = { ...project_pages };
   } else {
-    if (env.VITE_APP_MODEL)
-      pages[env.VITE_APP_MODEL] = project_pages[env.VITE_APP_MODEL];
+    if (env.VITE_APP_MODEL) pages[env.VITE_APP_MODEL] = project_pages[env.VITE_APP_MODEL];
     else pages = { ...project_pages };
   }
   return {
-    plugins: [vue()],
+    plugins: [vue(), eslint()],
     build: {
       manifest: true, // 生成 manifest.json 文件
       rollupOptions: {
         input: pages,
-        output: { dir: "./dist" },
+        output: { dir: './dist' },
       },
     },
     resolve: {
-      extensions: [".js", ".ts", ".vue", ".json"], // 导入时可以省略的扩展名
+      extensions: ['.js', '.ts', '.vue', '.json'], // 导入时可以省略的扩展名
       alias: {
-        "@": resolve(__dirname, "src"),
-        "@page1": resolve(__dirname, "src/pages/page1"),
-        "@page2": resolve(__dirname, "src/pages/page2"),
+        '@': resolve(__dirname, 'src'),
+        '@page1': resolve(__dirname, 'src/pages/page1'),
+        '@page2': resolve(__dirname, 'src/pages/page2'),
       },
     },
   };
